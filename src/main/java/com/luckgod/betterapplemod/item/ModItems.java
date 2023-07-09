8166fb40d3fe3cdaca00e0a108eaa7459e65166f
ImperfectCoder0 <sriniranjan.gs@gmail.com>
2023-07-09 11:15:52 -0400

    Initial commit

---
+++ src/main/java/com/luckgod/betterapplemod/item/ModItems.java
@@ -0,0 +1,35 @@
+package com.luckgod.betterapplemod.item;
+
+import com.luckgod.betterapplemod.BetterApplesMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.FoodComponents;
+import net.minecraft.item.Item;
+
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item IRON_APPLE = registerItem("iapple", new IApple(new FabricItemSettings().food(IApple.IAppleFood)));
+    public static final Item DIAMOND_APPLE = registerItem("dapple", new DApple(new FabricItemSettings().food(DApple.DAppleFood)));
+
+    private static void addItemsToFoodsTabItemGroup(FabricItemGroupEntries entries){
+        entries.add(IRON_APPLE);
+        entries.add(DIAMOND_APPLE);
+    }
+
+    public static void registerModItems(){
+        BetterApplesMod.LOGGER.info("Mod items registered baby! " + BetterApplesMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToFoodsTabItemGroup);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(BetterApplesMod.MOD_ID, name), item);
+
+    }
+}
